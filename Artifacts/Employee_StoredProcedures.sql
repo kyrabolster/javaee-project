33667f333ddb09cb58f7c32a869211214ec05b8c
kyrabolster <49887176+kyrabolster@users.noreply.github.com>
2021-03-11 20:01:23 -0400

    Employee Retrieve Stored Procs created

--- Artifacts/Employee_StoredProcedures.sql
+++ Artifacts/Employee_StoredProcedures.sql
@@ -1,3 +1,9 @@
+USE `ats2021hotel`;
+
+DELIMITER //
+DROP PROCEDURE IF EXISTS InsertEmployee;
+// DELIMITER;
+
 DELIMITER $$
 CREATE PROCEDURE `InsertEmployee` (
   IN FirstName_param VARCHAR(50),
@@ -7,9 +13,42 @@ CREATE PROCEDURE `InsertEmployee` (
   OUT Id_out INT
 )
 BEGIN
-INSERT INTO employees(FirstName, LastName, SIN, HourlyRate, IsDeleted, CreatedAt)
-    VALUES(FirstName_param, LastName_param, SIN_param, HourlyRate_param, 0, CURRENT_TIMESTAMP());
-    
+	INSERT INTO employees(FirstName, LastName, SIN, HourlyRate, IsDeleted, CreatedAt)
+		VALUES(FirstName_param, LastName_param, SIN_param, HourlyRate_param, 0, CURRENT_TIMESTAMP());
+		
     SET Id_out = LAST_INSERT_ID();
+END$$
+DELIMITER ;
+
+DELIMITER //
+DROP PROCEDURE IF EXISTS RetrieveAllEmployees;
+// DELIMITER;
+
+DELIMITER $$
+CREATE PROCEDURE `RetrieveAllEmployees` ()
+BEGIN
+
+	SELECT (Id, FirstName, LastName, SIN, HourlyRate, IsDeleted, CreatedAt, UpdatedAt, DeletedAt)
+    FROM employees
+    ORDER BY CreatedAt;
+
+END$$
+DELIMITER ;
+
+DELIMITER //
+DROP PROCEDURE IF EXISTS RetrieveAllEmployeeById;
+// DELIMITER;
+
+DELIMITER $$
+CREATE PROCEDURE `RetrieveAllEmployeeById` (
+	IN Id_param INT
+)
+BEGIN
+
+	SELECT (Id, FirstName, LastName, SIN, HourlyRate, IsDeleted, CreatedAt, UpdatedAt, DeletedAt)
+    FROM employees
+    WHERE (Id_param IS NULL OR id = Id_param)
+    ORDER BY CreatedAt;
+
 END$$
 DELIMITER ;
