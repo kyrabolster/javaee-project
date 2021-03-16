def887e89a0bac375b248dfd83521eb54a537930
soyoungkim1123 <59565605+soyoungkim1123@users.noreply.github.com>
2021-03-16 17:13:56 -0300

    working on task repo

--- com.nbcc.ATSsystem/src/com/nbcc/ATSsystem/repository/TaskRepository.java
+++ com.nbcc.ATSsystem/src/com/nbcc/ATSsystem/repository/TaskRepository.java
@@ -6,21 +6,27 @@
 package com.nbcc.ATSsystem.repository;
 
 import com.nbcc.ATSsystem.models.ITask;
+import com.nbcc.ATSsystem.models.TaskFactory;
 import com.nbcc.dataaccess.DALFactory;
 import com.nbcc.dataaccess.IDAL;
+import com.nbcc.dataaccess.IParameter;
+import com.nbcc.dataaccess.ParameterFactory;
+import java.sql.SQLException;
 import java.util.List;
+import javax.sql.rowset.CachedRowSet;
 
 /**
  *
  * @author Soyoung Kim
  * @date 2021-03-15
  */
-public class TaskRepository  extends BaseRepository implements ITaskRepository{
+public class TaskRepository extends BaseRepository implements ITaskRepository {
+
     private final String SPROC_SELECT_TASKS = "CALL RetrieveAllTasks";
     private final String SPROC_SELECT_TASK = "CALL RetrieveTasks(?)";
-    
+
     private IDAL dataAccess;
-    
+
     public TaskRepository() {
         dataAccess = DALFactory.createInstance();
     }
@@ -42,11 +48,46 @@ public class TaskRepository  extends BaseRepository implements ITaskRepository{
 
     @Override
     public List<ITask> retrieveTasks() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        List<ITask> retrievedTasks = TaskFactory.createListInstance();
+
+        try {
+            CachedRowSet cr = dataAccess.executeFill(SPROC_SELECT_TASKS, null);
+            retrievedTasks = toListofTasks(cr);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        
+        return retrievedTasks;
     }
 
     @Override
     public ITask retrieveTask(int id) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        List<ITask> tasks = TaskFactory.createListInstance();
+
+        try {
+            List<IParameter> params = ParameterFactory.createListInstance();
+            CachedRowSet cr = dataAccess.executeFill(SPROC_SELECT_TASK, params);
+            tasks = toListofTasks(cr);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        
+        return tasks.get(0);
+    }
+
+    private List<ITask> toListofTasks(CachedRowSet cs) throws SQLException {
+        List<ITask> retrievedTasks = TaskFactory.createListInstance();
+        ITask task;
+
+        while (cs.next()) {
+            task = TaskFactory.createInstance();
+            task.setId(super.getInt("id", cs));
+            task.setName(cs.getString("Name"));
+            task.setDescription(cs.getString("Description"));
+            task.setDuration(super.getInt("Duration", cs));
+            retrievedTasks.add(task);
+        }
+        
+        return retrievedTasks;
     }
 }
