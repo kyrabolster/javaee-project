a07271758839eb73ad66eef569197c5198a80f4f
soyoungkim1123 <59565605+soyoungkim1123@users.noreply.github.com>
2021-03-19 22:44:51 -0300

    added create team stored procedure

---
+++ Artifacts/Team_StoredProcedures.sql
@@ -0,0 +1,28 @@
+DELIMITER $$
+CREATE DEFINER=`dev`@`%` PROCEDURE `InsertTeam`(
+IN Name_param NVARCHAR(255),
+IN IsOnCall_param BIT,
+IN Employee1_param INT,
+IN Employee2_param INT,
+OUT Id_out INT
+)
+BEGIN
+	DECLARE exit handler FOR SQLEXCEPTION, SQLWARNING
+	  BEGIN
+		ROLLBACK;
+		RESIGNAL;
+	  END;
+
+	START TRANSACTION; 
+		INSERT INTO teams(Name, IsOnCall, IsDeleted, CreatedAt)
+		VALUES(Name_param, IsOnCall_param, 0, CURRENT_TIMESTAMP());
+		
+		SET Id_out = LAST_INSERT_ID();
+		
+		INSERT INTO team_members(EmployeeId, TeamId)
+		VALUES
+			(Employee1_param, Id_out),
+			(Employee2_param, Id_out);
+	COMMIT;
+END$$
+DELIMITER ;
